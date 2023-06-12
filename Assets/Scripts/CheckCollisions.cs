e4d9676e93b4df0640d903e57075bcaed0d6b065
koray <105800358+KorayKaraman1@users.noreply.github.com>
2023-06-12 21:29:33 +0300

    Coin eklendi

---
+++ Assets/Scripts/CheckCollisions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckCollisions : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI coinText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            other.gameObject.SetActive(false);
+        }
+    }
+    public void AddCoin()
+    {
+        score++;
+        coinText.text="Score: "+ score.ToString();
+    }
+}
